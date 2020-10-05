3470c4b9fd69f1ff24b653e6cfbfa9baf9a2ff0a
Sandip-android16 <62545767+Sandip-android16@users.noreply.github.com>
2020-10-05 13:55:03 +0530

    Update MainActivity.java

--- app/src/main/java/org/snowcorp/imageupload/MainActivity.java
+++ app/src/main/java/org/snowcorp/imageupload/MainActivity.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,13 +17,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.error.AuthFailureError;
 import com.android.volley.error.VolleyError;
 import com.android.volley.request.SimpleMultiPartRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
@@ -119,9 +120,20 @@ public class MainActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
             }
-        });
+        })
+        {
 
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> pars = new HashMap<String, String>();
+                pars.put("Content-Type", "application/x-www-form-urlencoded");
+                
+                return pars;
+            }
+        };
         smr.addFile("image_url", imagePath);
+        smr.addStringParam("name","data");
         MyApplication.getInstance().addToRequestQueue(smr);
 
     }
